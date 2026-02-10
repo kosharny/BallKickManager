15a5725caa15837add4198f2a6ec242fbc191a47
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-10 15:17:23 +0200

    Initial Commit

---
+++ BallKickManager/BallKickManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallKickManagerApp.swift
+//  BallKickManager
+//
+//  Created by Maksim Kosharny on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BallKickManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
